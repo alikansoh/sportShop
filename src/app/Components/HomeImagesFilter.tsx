e632a172dcf8d1a83cab5ba9bc97ed4728aaa87e
alikansoh <alikansoh.dev@gmail.com>
2025-01-14 23:23:02 +0000

    add

--- src/app/Components/HomeImagesFilter.tsx
+++ src/app/Components/HomeImagesFilter.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-const CategoryButton = ({ label }) => (
+const CategoryButton = ({ label }: { label: string }) => (
     <div className='flex flex-col'>
         <div className='border-[1px] rounded-md border-black h-[3rem] justify-center items-center mb-6 flex cursor-pointer p-7 py-5 text-nowrap mobile:p-6 mobile:py-3'>
             <p className='text-center text-sm font-normal'>{label}</p>
