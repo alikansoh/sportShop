e4ae31420cd39ff9fd042eb18d106b7fcda110c9
alikansoh <alikansoh.dev@gmail.com>
2025-01-15 22:28:25 +0000

    added header image

--- src/app/Components/Hero.tsx
+++ src/app/Components/Hero.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 function Hero() {
   return (
     <div className=' w-full'>
-      <Image src="/images/header.png" alt="heroImage" width={10000} height={10000} className='w-full h-[630px] mobile:h-[420px]' />
+      <Image src="/images/header.jpeg" alt="heroImage" width={10000} height={10000} className='w-full h-[900px] mobile:h-[420px]' />
 
     </div>)
 }
