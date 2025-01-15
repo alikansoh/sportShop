9414ed67fa0de5a3092c472a1d6bdba7c82e4a19
alikansoh <alikansoh.dev@gmail.com>
2025-01-15 19:51:33 +0000

    add

--- src/app/Components/HomeImagesFilter.tsx
+++ src/app/Components/HomeImagesFilter.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 const CategoryButton = ({ label }: { label: string }) => (
     <div className='flex flex-col'>
-        <div className='border-[1px] rounded-md border-black h-[3rem] justify-center items-center mb-6 flex cursor-pointer p-7 py-5 text-nowrap mobile:p-6 mobile:py-3 '>
+        <div className='border-[1px] rounded-md border-black h-[3rem] justify-center items-center mb-6 flex cursor-pointer p-7 py-5 text-nowrap mobile:p-6 mobile:py-3 mobile:no-scrollbar tablet:no-scrollbar'>
             <p className='text-center text-sm font-normal'>{label}</p>
         </div>
-    </div>mobile:no-sc
+    </div>
 );
 
 export const HomeImagesFilter = () => {
     const categories = ['Mens', 'Womens', 'Kids', 'Trainers', 'Football', 'Running', 'Training', 'Daily Deal'];
 
     return (
-<div className="flex flex-row gap-2 justify-center mobile:justify-start mobile:overflow-x-hidden tablet:overflow-x-scroll tablet:justify-start tablet:mx-3 scrollbar custom-scrollbar lg:mx-2">
+        <div className='flex flex-row gap-2 justify-center mobile:justify-start mobile:overflow-x-scroll mobile:mx-3 tablet:overflow-x-scroll tablet:justify-start tablet:mx-3 scrollbar- custom-scrollbar lg:mx-2 ' >
             {categories.map((category) => (
                 <CategoryButton key={category} label={category} />
             ))}
