3d44894d013e37caea82b9d1f560750a2084ab5a
alikansoh <alikansoh.dev@gmail.com>
2025-02-02 13:52:47 +0000

    carusel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Hero from "./Components/Hero";
 import PopularItem from "./Components/PopularItem";
 import { HomeImagesFilter } from "./Components/HomeImagesFilter";
-import Carousel from "./Components/Carousel";
 export default function Home() {
   return (
     <div className="flex flex-col">
