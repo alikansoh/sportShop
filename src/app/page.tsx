6798739f65b74c23b7033358f1e8905c15c7af28
alikansoh <alikansoh.dev@gmail.com>
2025-02-08 15:37:45 +0000

    new update and changing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Hero from "./Components/Hero";
 import PopularItem from "./Components/PopularItem";
 import { HomeImagesFilter } from "./Components/HomeImagesFilter";
-import Deal from "./Components/Deal";
+import Deal from "./Components/Deal.jsx";
 export default function Home() {
   return (
     <div className="flex flex-col font-sans">
